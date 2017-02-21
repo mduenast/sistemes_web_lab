504a089ee41e7e928d0af20d56423aea6e6c483e
naluem <mdt_ps32468@hotmail.com>
2017-02-21 12:25:56 +0100

    data sense format

--- sistemes_web_lab1.py
+++ sistemes_web_lab1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import urllib2
+from bs4 import BeautifulSoup
 
 '''
 Client web per www.udl.cat
@@ -8,8 +9,8 @@ Client web per www.udl.cat
 
 class Client(object):
 
-        def __init__(self):
-            pass
+        def __init__(self,url):
+            self.url = url
 
         def get_web(self,page):
             """ baixar-se la web """
@@ -17,13 +18,25 @@ class Client(object):
             html = file.read()
             file.close()
             return html
+
+        # TODO: buscar el text
+        def search(self,html):
+            soup = BeautifulSoup(html,'html.parser')
+            elements = soup.find_all("div","featured-links-item")
+            resultats = []
+            for element in elements:
+                data = element.find("time")["datetime"]
+                resultats.append(data)
+            return resultats
+
         def main(self):
             # buscar el text
             # imprimir resultats
-            web = self.get_web("http://www.udl.cat")
-            print web
-            pass
+            web = self.get_web(self.url)
+            resultat = self.search(web)
+            #FIXME: imprimir resultats
+            print resultat
 
 if __name__ == "__main__":
-    client = Client()
+    client = Client("http://www.udl.cat")
     client.main()
